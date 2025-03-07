1775f3718691d3aed4632a56629d13645f7ac927
Kalwa29 <166616001+Kalwa29@users.noreply.github.com>
2025-03-07 20:48:03 -0300

    Add files via upload
    aula03_tarefas01.02.03.py

---
+++ aula03_tarefa02.py
@@ -0,0 +1,18 @@
+a = 10
+b = 5
+soma = a + b
+subtração = a - b
+multiplicacao = a * b
+divisao_real = a / b
+divisao_inteira = a // b
+modulo = a % b
+exponenciacao = a ** b
+
+print(f"o resultado da soma é {soma}")
+print(f"o resultado da soma é {subtração}")
+print(f"o resultado da soma é {divisao_real}")
+print(f"o resultado da soma é {divisao_inteira}")
+print(f"o resultado da soma é {modulo}")
+print(f"o resultado da soma é {exponenciacao}")
+
+
---
+++ aula03_tarefa03.py
@@ -0,0 +1,19 @@
+a=input("Digite o primeiro numero :")
+a=float(a)
+b=input("Digite o segundo numero :")
+b=float(b)
+
+soma = a + b
+subtração = a - b
+multiplicacao = a * b
+divisao_real = a / b
+divisao_inteira = a // b
+modulo = a % b
+exponenciacao = a ** b
+
+print(f"o resultado da soma é {soma}")
+print(f"o resultado da subtracao é {subtração}")
+print(f"o resultado da divisao_real é {divisao_real}")
+print(f"o resultado da divisao_inteira é {divisao_inteira}")
+print(f"o resultado da modulo é {modulo}")
+print(f"o resultado da expoenciacao é {exponenciacao}")
