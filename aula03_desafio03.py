af46af8ec2379665d810e9f6925e90d529303156
Kalwa29 <166616001+Kalwa29@users.noreply.github.com>
2025-03-07 21:21:43 -0300

    Add files via upload
    aula03_desafio03.py

---
+++ aula03_desafio03.py
@@ -0,0 +1,9 @@
+a=input("Me de a primeira nota :")
+a = float(a)
+b=input("Me de a segunda nota :")
+b = float(b)
+c=input("Me de a terceira nota :")
+c = float(c)
+
+media = (a + b + c)/3
+print(f"a media das tres notas é :{media:.2f}")
