515443ce61a227164e11ca8ee402a4f74184dc7d
Kalwa29 <166616001+Kalwa29@users.noreply.github.com>
2025-03-07 21:47:12 -0300

    Add files via upload
    aula04_tarefa01.py

---
+++ aula04_tarefa01.py
@@ -0,0 +1,23 @@
+x = 10
+print(f"O valor inicial de x é {x}")
+
+x+=5
+print(f"O valor da soma de x é {x} ")
+
+x-=3
+print(f"O valor da subtracao de x é {x} ")
+
+x*=2
+print(f"O valor da multiplicacao de x é {x} ")
+
+x/=4
+print(f"O valor da divisao de x é {x} ")
+
+x//=3
+print(f"O valor da divisao_inteira de x é {x} ")
+
+x%=2
+print(f"O valor do modulo de x é {x} ")
+
+x**=3
+print(f"O valor da exponenciacao de x é {x} ")
