810c4785f91a55ceef042c171219ac03a87965d7
Kalwa29 <166616001+Kalwa29@users.noreply.github.com>
2025-03-08 16:17:17 -0300

    Add files via upload
    aula04_desafio03_para_casa

---
+++ aula04_desafio03_para_casa_correto.py
@@ -0,0 +1,23 @@
+x = input("Me de um numero inteiro :")
+print(f"O valor inicial de x é {x}")
+
+x = int(x) + 5
+print(f"O valor da soma do numero inteiro é {x} ")
+
+x=int(x) - 3
+print(f"O valor da subtracao do numero inteiro é {x} ")
+
+x=int(x) * 2
+print(f"O valor da multiplicacao do numero inteiro é {x} ")
+
+x=int(x) / 4
+print(f"O valor da divisao do numero inteiro é {x} ")
+
+x=int(x) // 3
+print(f"O valor da divisao_inteira do numero inteiro é {x} ")
+
+x=int(x) % 2
+print(f"O valor do modulo do numero inteiro é {x} ")
+
+x=int(x) ** 3
+print(f"O valor da exponenciacao do numero inteiro é {x} ")
